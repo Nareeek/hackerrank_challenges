236ad876f6167d6c0c2af33de5629312e6bcf112
Narek <narek.ikhtiaryan.ni@gmail.com>
2021-06-09 02:22:51 -0700

    add Level_order_traversal.cpp solution

---
+++ Tree/Level_order_traversal/Level_order_traversal.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>	
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+
+    void levelOrder(Node * root) {
+        queue<Node*> q;
+        q.push(root);
+        
+        Node* front;
+        while (!q.empty()) {
+            front = q.front();
+            q.pop();
+            
+            cout << front->data << ' ';
+            if(front->left){
+                q.push(front->left);
+            }
+            
+            if(front->right){
+                q.push(front->right);
+            } 
+        }
+    }
+
+}; //End of Solution
+
+int main() {
+  
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+	myTree.levelOrder(root);
+    return 0;
+}
+
